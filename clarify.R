6e1526ab6180c4539e9c30be9ceee7f7d3ece642
dbreynol <david.reynolds@unh.edu>
2022-04-21 10:35:51 -0400

    shrek stuff

--- clarify.R
+++ clarify.R
@@ -6,6 +6,15 @@ plot(lap[,3], main = "Cardiovascular Mortality")
 decomp = decompose(lap[,3])
 acf(diff_52, type = "partial", lag.max = 104)
 
-mod = sarima(lap[,3], 2, 0, 0, 1, 1, 0, S = 52)
+
+
+mod1 = sarima(lap[,1], 2, 0, 0, 1, 1, 0, S = 52, xreg = cbind(lap[,11], lap[,10]) )
+mod2 = sarima(lap[,1], 2, 0, 0, 1, 1, 0, S = 52)
+
+
+
 sarima.for(lap[,3], 52, 2, 0, 0, 1, 1, 0, 52)
+
+
+
 
