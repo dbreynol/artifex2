f2c8d14ace9252005cac1db5dcf57fadaba13ae8
dbreynol <david.reynolds@unh.edu>
2022-04-26 08:02:20 -0400

    team nba

--- clarify.R
+++ clarify.R
@@ -15,6 +15,32 @@ mod2 = sarima(lap[,1], 2, 0, 0, 1, 1, 0, S = 52)
 
 sarima.for(lap[,3], 52, 2, 0, 0, 1, 1, 0, 52)
 
+library(tidyverse)
+nba.dat = read.csv('Team_stats_per_game.csv')
 
+
+# filter to start in 1980 (when 3 pointers started)
+# and create a response variable - 3 point attempts, averaged across teams
+# and predictor - mean 3 point conversion pct.
+nba.processed = nba.dat %>% 
+  filter(season > 1979) %>%
+  group_by(season) %>%
+  summarise(y = mean(x3pa_per_game), x = mean(x3p_percent))
+
+# make the response into a time series object - which clearly has an increasing trend
+# inspect the lag 1 differences for stationarity - looks good
+yt = as.ts(nba.processed$y, start = 1980, end = 2022)
+plot(diff(yt))
+(acf(yt, type = "partial")) # order one AR model seems appropriate
+
+
+m0 = arima(yt, order = c(1, 1, 0)) # fit AR model with one past period to the differenced data
+predict(m0, n.ahead = 5) # predict 5 years into the future
+
+
+
+xt = as.ts(nba.processed$x, start = 1980, end = 2022)
+m1 = arima(yt, order = c(1, 1, 0), xreg = xt)
+predict(m1, n.ahead = 5, newxreg = window(xt, start = 39))
 
 
